95c24dfc0387ab3e6320f4597a6bcbb86fdfb01c
Stefano <stefanog2002@yahoo.com>
2026-03-25 22:45:08 -0400

    removed comments and unecessary hooks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,24 +3,6 @@ import Login from './Login/Login'
 
 function App() {
 
-  const [items, setItems] = useState({});
-  const [dataIsLoaded, setDataIsLoaded] = useState(false);
-
-  // useEffect(() => {
-  //   fetch("http://localhost:8000/api/users/me", {
-  //     method: 'GET',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwiZXhwIjoxNzc0NDgyNzk3fQ.gzeGg4ZW3znqvCHIFMFUrFJ3LkDhEmk5BpBLmeKCXTM'
-  //     }
-  //   })
-  //   .then((res) => res.json())
-  //   .then((json) => {
-  //     setItems(json);
-  //     setDataIsLoaded(true);
-  //   })
-  // }, []);
-
   return(
     <>
       <Login/>
